fe5f0cd20e1ffd0b3bb56b9dc841ea349a49495a
Romain Beaumont <romain.rom1@gmail.com>
2018-11-03 22:59:15 +0100

    circle ci + standard

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # node-minecraft-assets
 [![NPM version](https://badge.fury.io/js/minecraft-assets.svg)](http://badge.fury.io/js/minecraft-assets)
 [![Tonic](https://img.shields.io/badge/tonic-try%20it-blue.svg)](https://tonicdev.com/npm/minecraft-assets)
+[![Build Status](https://circleci.com/gh/PrismarineJS/node-minecraft-assets/tree/master.svg?style=shield)](https://circleci.com/gh/PrismarineJS/node-minecraft-assets/tree/master)
 
 Provide easy access to [minecraft-assets](https://github.com/rom1504/minecraft-assets) in node.js
 
 ## Example
 
 ```js
-var mcAssets=require("minecraft-assets")("1.8.8");
+const mcAssets=require("minecraft-assets")("1.8.8")
 
-console.log("https://raw.githubusercontent.com/rom1504/minecraft-assets/master/data/1.8.8/"+mcAssets.getTexture("wheat_seeds")+".png");
+console.log("https://raw.githubusercontent.com/rom1504/minecraft-assets/master/data/1.8.8/"+mcAssets.getTexture("wheat_seeds")+".png")
 
-console.log(mcAssets.textureContent["wheat_seeds"].texture);
+console.log(mcAssets.textureContent["wheat_seeds"].texture)
 ```
 
 ## Documentation
--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-var mcDataToNode=require("./lib/loader");
-var cache={}; // prevent reindexing when requiring multiple time the same version
+const mcDataToNode = require('./lib/loader')
+const cache = {} // prevent reindexing when requiring multiple time the same version
 
+module.exports = function (mcVersion) {
+  if (cache[mcVersion]) { return cache[mcVersion] }
+  const mcData = data[mcVersion]
+  if (mcData == null) { return null }
+  const nmcData = mcDataToNode(mcData, mcVersion)
+  cache[mcVersion] = nmcData
+  return nmcData
+}
 
-module.exports = function(mcVersion)
-{
-  if(cache[mcVersion])
-    return cache[mcVersion];
-  var mcData=data[mcVersion];
-  if(mcData==null)
-    return null;
-  var nmcData=mcDataToNode(mcData,mcVersion);
-  cache[mcVersion]=nmcData;
-  return nmcData;
-};
-
-var data={
-  "1.8.8":{
-    blocksTextures:require("./minecraft-assets/data/1.8.8/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.8.8/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.8.8/texture_content")
+const data = {
+  '1.8.8': {
+    blocksTextures: require('./minecraft-assets/data/1.8.8/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.8.8/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.8.8/texture_content')
   },
-  "1.9":{
-    blocksTextures:require("./minecraft-assets/data/1.9/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.9/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.9/texture_content")
+  '1.9': {
+    blocksTextures: require('./minecraft-assets/data/1.9/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.9/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.9/texture_content')
   },
-  "1.10":{
-    blocksTextures:require("./minecraft-assets/data/1.10/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.10/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.10/texture_content")
+  '1.10': {
+    blocksTextures: require('./minecraft-assets/data/1.10/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.10/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.10/texture_content')
   },
-  "1.11.2":{
-    blocksTextures:require("./minecraft-assets/data/1.11.2/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.11.2/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.11.2/texture_content")
+  '1.11.2': {
+    blocksTextures: require('./minecraft-assets/data/1.11.2/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.11.2/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.11.2/texture_content')
   },
-  "1.12":{
-    blocksTextures:require("./minecraft-assets/data/1.12/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.12/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.12/texture_content")
+  '1.12': {
+    blocksTextures: require('./minecraft-assets/data/1.12/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.12/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.12/texture_content')
   },
-  "1.13":{
-    blocksTextures:require("./minecraft-assets/data/1.13/blocks_textures"),
-    itemsTextures:require("./minecraft-assets/data/1.13/items_textures"),
-    textureContent:require("./minecraft-assets/data/1.13/texture_content")
+  '1.13': {
+    blocksTextures: require('./minecraft-assets/data/1.13/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.13/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.13/texture_content')
   }
-};
+}
